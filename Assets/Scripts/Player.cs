201995695e52576b76aef177111b6cd78aff2c06
vinicinho2007 <mreisloures123@gmail.com>
2023-07-21 22:16:46 -0300

    animation jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,16 @@ using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
+    [Header("Config.Movement e Jump")]
+    public BoxCollider2D boxCollider2D;
     public Rigidbody2D rig2D;
     public float speed, speedRun, forceJump;
+    public Vector2 friction;
     private float _speedCurrent;
+
+    [Header("Animation Jump")]
+    public Vector2 scaleJump;
+    public float animDuration;
 
     private void FixedUpdate()
     {
@@ -37,19 +44,28 @@ public class Player : MonoBehaviour
 
         if (rig2D.velocity.x > 0)
         {
-            rig2D.velocity += new Vector2(-.1f, 0);
+            rig2D.velocity += friction;
         }
         else if (rig2D.velocity.x < 0)
         {
-            rig2D.velocity -= new Vector2(-.1f, 0);
+            rig2D.velocity -= friction;
         }
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rig2D.velocity = Vector2.up * forceJump;
+            rig2D.velocity = transform.up * forceJump;
+            AnimationJumpTo();
         }
+    }
+
+    private void AnimationJumpTo()
+    {
+        transform.localScale = Vector2.one;
+        DOTween.Kill(transform);
+        transform.DOScaleX(scaleJump.x, animDuration).SetLoops(2,LoopType.Yoyo).SetEase(Ease.OutBack);
+        transform.DOScaleY(scaleJump.y, animDuration).SetLoops(2,LoopType.Yoyo).SetEase(Ease.OutBack);
     }
 }
