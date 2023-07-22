fac7d81e2aa065a356bc175e1e2004faf9691e79
vinicinho2007 <mreisloures123@gmail.com>
2023-07-21 22:55:41 -0300

    add gameover

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,13 +6,15 @@ using DG.Tweening;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instante;
-    public GameObject prefPlayer;
+    public GameObject prefPlayer, gameOver;
     public Transform position;
-    public float animDuration;
+    public float animDuration, delayOpenGameOver, animDurationGameOver;
+    private List<GameObject> objs;
 
     private void Start()
     {
-        if(instante == null)
+        objs = new List<GameObject>();
+        if (instante == null)
         {
             instante = this;
         }
@@ -27,5 +29,24 @@ public class GameManager : MonoBehaviour
     {
         GameObject obj = Instantiate(prefPlayer, position);
         obj.transform.DOScale(0, animDuration).From();
+    }
+
+    public IEnumerator GameOver()
+    {
+        gameOver.SetActive(true);
+        foreach (Transform obj in gameOver.GetComponentsInChildren<Transform>())
+        {
+            if (obj != gameOver.transform&&obj.gameObject.activeInHierarchy)
+            {
+                objs.Add(obj.gameObject);
+                obj.gameObject.SetActive(false);
+            }
+        }
+        yield return new WaitForSeconds(delayOpenGameOver);
+        for(int i =0;i<objs.Count;i++)
+        {
+            objs[i].gameObject.SetActive(true);
+            objs[i].transform.DOScale(0, animDurationGameOver).From();
+        }
     }
 }
