c9dbf9a050e8b233cf0a014b530b90e9e41a1da8
vinicinho2007 <mreisloures123@gmail.com>
2023-08-20 18:37:17 -0300

    add particule system munition

--- Assets/Scripts/Gun/Munition_Add.cs
+++ Assets/Scripts/Gun/Munition_Add.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Munition_Add : MonoBehaviour
 {
+    public ParticleSystem particleSystemMunition;
     public SOFloat munition;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            particleSystemMunition.transform.SetParent(null);
+            particleSystemMunition.Play();
             GameObject.FindObjectOfType<Gun>()._maxMunition += munition.value;
             Destroy(gameObject);
         }
